a704e34ff0bc440092b84d5bee18654b32080cc5
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-07-24 17:35:01 -0500

    First Tweet function made so you can tweet the initial prices when you start monitoring for any amount of coins

--- cryptobot.py
+++ cryptobot.py
@@ -4,12 +4,16 @@ import tweepy
 from json import loads
 from requests import get
 from mgconfig import *
+from threading import Thread
+from time import sleep
 
 
 # Authenticating with Tweepy
 auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
+
+#### NEED TO FIND A BETTER WAY OF MAKING THE COIN LIST
 
 # The URLS are links to json objects to the price of a currency in terms of a specified currency
 # The json objects will have the price in USD because syms=USD
@@ -20,14 +24,39 @@ ltc = ('LTC', 'https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD')
 xrp = ('XRP', 'https://min-api.cryptocompare.com/data/price?fsym=XRP&tsyms=USD') # Ripple
 sc  = ('SC', 'https://min-api.cryptocompare.com/data/price?fsym=SC&tsyms=USD')   # SiaCoin
 
+coin_list = [btc, eth];
+
 # Returns the price for any coin given as an arugment.
-# Returns in this format: {'USD': 2764} (json object)
+# Returns in this format: {'USD': 2764} (dictionary)
 def coin_price(coin): 
 	price = loads(get(coin[1]).text) 
 	return price
 
+
+# A function that monitors the price fluctuation of a coin and tweets if there is a certain 
+# percent change. 
 def percent_monitor(coin):
 	coin_price(coin)
-	
-btcprice = coin_price(btc);
-print(btcprice)
+	### Need to write something that checks every minute or two and then tweets if the change is large enough
+
+# Tweets the price of all desired coins at that time
+def first_tweet(coin_list):
+	message = 'Crypto Prices right now:\n'
+	for coin in coin_list:
+		price = coin_price(coin)
+		message = message + '\n' + coin[0] + ': ' + str(price['USD'])
+	api.update_status(message)
+	# coin[0] will print out the cyrptocurrency code for that coin: 'BTC'
+	# price['BTC'] will give the dictionary price value for BTC if price = get_price(btc)
+
+# The function that will start the monitoring of all specified coins
+def monitor_coins(coin_list):
+	first_tweet(coin_list)
+	sleep(60)
+	# Start monitoring each coin
+	for coin in coin_list:
+		thread = Thread(target = percent_monitor(coin))
+		thread.start()
+
+
+first_tweet(coin_list)
