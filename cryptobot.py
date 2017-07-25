beeb60f4f7089f7db0fab2edef3c4e8a43f850b6
Gabe Rojas-Westall <rojaswestall@gmail.com>
2017-07-24 21:54:31 -0500

    change_monitor should be working and bot should be fully functional\!

--- cryptobot.py
+++ cryptobot.py
@@ -25,7 +25,7 @@ eth = ('ETH', 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD')
 ltc = ('LTC', 'https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD') # Litecoin
 xrp = ('XRP', 'https://min-api.cryptocompare.com/data/price?fsym=XRP&tsyms=USD') # Ripple
 sc  = ('SC', 'https://min-api.cryptocompare.com/data/price?fsym=SC&tsyms=USD')   # SiaCoin
-coin_list = [btc, eth];
+coin_list = [btc, eth, ltc, xrp, sc];
 
 
 # Returns the price for any coin given as an arugment.
@@ -43,19 +43,35 @@ def percent_change(original, new):
 
 # A function that monitors the price fluctuation of a coin and tweets if there is a certain 
 # percent change. 
-def percent_monitor(coin):
+def change_monitor(coin):
+	ogtime = datetime.now()
 	old_price = coin_price(coin)['USD']
 	new_price = old_price
 	# Check if price change is greater than 5%. 
 	# If it's not, wait a minute, update the new prices, and try again
-	while percentchange(old_price, new_price) < 5:
+	while percent_change(old_price, new_price) < 5:
 		sleep(60)
 		new_price = coin_price(coin)['USD']
-	# If it's greater tweet the change, whether it increased or decreased, and the period of time it took
+		#### Want to reset the old price every 12 hours regardless of whether or not it has shifted by 5%
+		#### because 5% differences after 12 hours aren't as significant
+		####timedif = ogtime - datetime.now()
+		####if timedif.seconds > 43200: # 43200sec = 12 hours
+		####	ogtime = datetime.now()
+	# If it's greater, tweet the change, whether it increased or decreased, and the period of time it took
+	timedif = ogtime - datetime.now()
+	hours = int(timedif.seconds / 3600) # The number of hours (-minutes) it took to change 5%
+	minutes = int((timedif.seconds - (hours * 3600)) / 60) # The number of minutes (-hours) it took to change 5%
 
+	if new_price > old_price:
+		message = 'has increased by 5%'
+	else:
+		message = 'has decreased by 5%'
 
-	# Want to reset the old price every day 12 hours regardless of whether or not it has shifted by 5%
-	# Tweet if it has dropped by 5% or increased by 5%
+	api.update_status(coin[0] + message + ' in the past ' + str(hours) + ' hours and ' + str(minutes) + ' minutes!\n\nThe new price is $' + str(new_price))
+	print('Price change tweeted for ' + coin[0])
+	change_monitor(coin)
+
+	# BUT HOW TO MAKE IT CONTINUOUSLY CHANGING???
 
 	### Need to write something that checks every minute or 
 	### two and then tweets if the change is large enough
@@ -63,13 +79,12 @@ def percent_monitor(coin):
 
 # Tweets the price of all desired coins at that time
 def first_tweet(coin_list):
-	rightnow = datetime.now().strftime("%b. %d %Y %I:%M%p")
+	rightnow = datetime.now().strftime("%b. %d, %Y %I:%M%p")
 	message = 'Crypto prices right now (' + rightnow + '):\n'
 	for coin in coin_list:
 		price = str(coin_price(coin)['USD'])
-		message = message + '\n' + coin[0] + ': ' + price
-	print(message)
-	#api.update_status(message)
+		message = message + '\n' + coin[0] + ': $' + price
+	api.update_status(message)
 	# coin[0] will print out the cyrptocurrency code for that coin: 'BTC'
 	# price['BTC'] will give the dictionary price value for BTC if price = get_price(btc)
 
@@ -79,8 +94,11 @@ def monitor_coins(coin_list):
 	first_tweet(coin_list)
 	# Start monitoring each coin
 	for coin in coin_list:
-		thread = Thread(target = percent_monitor(coin))
+		thread = Thread(target = change_monitor(coin))
 		thread.start()
 
-first_tweet(coin_list)
+#change_monitor(btc)
+
+monitor_coins(coin_list)
+
 
